efb0a69714ab53081eaab0f13e03c4f7ba3f6805
MarwaSayedHashem <moro.abdelnaby@gmail.com>
2025-05-29 09:50:47 +0300

    remove_is_authenticated()_to_is_authenticated

--- src/lorepo/editor/decorators.py
+++ src/lorepo/editor/decorators.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseForbidden
 
 def is_logged(function):
     def _wrapper(request, *args, **kwargs):
-        if not request.user.is_authenticated():
+        if not request.user.is_authenticated:
             return HttpResponseForbidden('You are logged out')
         return function(request, *args, **kwargs)
     return _wrapper
